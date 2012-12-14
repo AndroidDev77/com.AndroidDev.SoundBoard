5b7586bba9ad3a6891ee472518a778b6616368d7
Chris <Android Dev@192.168.2.4>
2012-12-14 10:33:32 -0500

    Added Icons

--- gen/com/murdersquirrel/newboard/R.java
+++ gen/com/murdersquirrel/newboard/R.java
@@ -11,9 +11,12 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int buttonbg=0x7f020000;
-        public static final int icon=0x7f020001;
-        public static final int mastbg=0x7f020002;
+        public static final int button=0x7f020000;
+        public static final int buttonbg=0x7f020001;
+        public static final int buttonpressed=0x7f020002;
+        public static final int icon=0x7f020003;
+        public static final int mastbg=0x7f020004;
+        public static final int selectorbutton=0x7f020005;
     }
     public static final class id {
         public static final int button01=0x7f070000;
